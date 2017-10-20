5245925213ba2e72f306a7d7f5d4dd1429475fcc
xiaoyuer <41465341@qq.com>
2017-10-20 20:36:54 +0800

    首次提交

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=jason',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+    'tablePrefix' => 'shop_',
+];
